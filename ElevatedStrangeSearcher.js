6ed019cda5396914f66e6496a99d7247a452ba0f
Dracnea <dracneagenie@gmail.com>
2021-03-21 02:01:10 -0400

    Update ElevatedStrangeSearcher.js

--- ElevatedStrangeSearcher.js
+++ ElevatedStrangeSearcher.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name TF2 Premium Search Elevated Stranges
-// @version 1.0
-// @description   A script used to make premium searching for elevated quality stranges on backpack.tf easier
-// @author        Dracnea
-// @match         https://backpack.tf/premium*
-// @grant         none
+// @name            TF2 Premium Search Elevated Stranges
+// @version         1.0
+// @description     A script used to make premium searching for elevated quality stranges on backpack.tf easier
+// @author          Dracnea
+// @match           https://backpack.tf/premium*
+// @grant           none
 // ==/UserScript==
 
 'use strict';
