acf9030e2e8853958f042a900e54e455086dc126
Dracnea <dracneagenie@gmail.com>
2021-03-21 02:00:44 -0400

    Create ElevatedStrangeSearcher.js

---
+++ ElevatedStrangeSearcher.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name TF2 Premium Search Elevated Stranges
+// @version 1.0
+// @description   A script used to make premium searching for elevated quality stranges on backpack.tf easier
+// @author        Dracnea
+// @match         https://backpack.tf/premium*
+// @grant         none
+// ==/UserScript==
+
+'use strict';
+//used to start the script
+(function () {
+    if(sessionStorage.startCheck == 1){
+        searcher();
+    } else {
+        gui();
+    }
+})();
